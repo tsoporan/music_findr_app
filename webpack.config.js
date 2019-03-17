56464ddd5bc109410719547750a51d06d73ac210
Titus Soporan <titus@tsoporan.com>
2019-03-16 21:49:41 -0400

    Added webpack dev server + config
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/index.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
