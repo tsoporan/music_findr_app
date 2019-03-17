63ac9aba361b613b07d10074d652e81f02bee8da
Titus Soporan <titus@tsoporan.com>
2019-03-17 12:41:27 -0400

    WIP Separate out YT client, basic signin working
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-import m from "mithril";
+import m from 'mithril'
 
-m.render(document.body, "Hello World");
+import yt from './clients/yt'
+
+const root = document.body
+
+const App = {
+  view: () => {
+    return m('main', [
+      m('h1', 'Music Findr'),
+      m(
+        'button',
+        {
+          onclick: function () {
+            yt.signIn()
+          }
+        },
+        'Sign in with Youtube'
+      ),
+      m(
+        'button',
+        {
+          onclick: function () {
+            yt.signOut()
+          }
+        },
+        'Sign out'
+      )
+    ])
+  }
+}
+
+m.mount(root, App)
