d001cdfd385aebb871c6ed5bf2c58fac05543d8f
Titus Soporan <titus@tsoporan.com>
2019-03-18 23:37:27 -0400

    Update webpack conf for css processing
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  mode: 'development',
+  mode: process.env.NODE_ENV || 'development',
   entry: {
     app: './src/index.js'
   },
@@ -9,7 +11,24 @@ module.exports = {
   devServer: {
     contentBase: './dist'
   },
-  plugins: [],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+      chunkFilename: 'styles.css'
+    }),
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: './index.html'
+    })
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
