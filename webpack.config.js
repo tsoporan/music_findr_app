bf6010495cf23215dec57131d6302bbc038629d8
Titus Soporan <titus@tsoporan.com>
2019-04-05 14:15:50 -0400

    Update webpack conf for babel
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,16 @@ module.exports = {
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       }
     ]
   },
